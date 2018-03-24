7c821a529c83f78298843f5be65dc87c4e36b34e
Student 241305 <czepim200@onet.pl>
2018-03-24 20:42:33 +0100

    Początek prac nad systemem kolizji

--- Arkanoid/Arkanoid/main.cpp
+++ Arkanoid/Arkanoid/main.cpp
@@ -10,6 +10,14 @@ unsigned const int windowSizeX = 1000;
 unsigned const int windowSizeY = 600;
 
 
+template <class Object1, class Object2> bool CheckeColision(Object1& obj1, Object2& obj2)
+{
+	return (obj1.Right() >= obj2.Left() &&
+			obj2.Right() >= obj1.Left() &&
+			obj1.Top() >= obj2.Bottom() &&
+			obj2.Bottom() >= obj1.Top() );
+}
+
 int main()
 {
 	Ball ball(windowSizeX/2, windowSizeY/2, windowSizeX, windowSizeY);	// TODO - dodaæ funkcje ustawiaj¹c¹ zmienne statyczne z wielkoœciami okna
